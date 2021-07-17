95920bad0dad494553ee85b44878af57a287ab1d
Roompuppy <86652628+Roompuppy@users.noreply.github.com>
2021-07-17 11:02:53 +0900

    학술회 7/16
    오투 학술회 2주차

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePosition();
+    }
+    void MovePosition()
+    {
+        this.transform.position = new Vector3(0, 5, 0);
+    }
+}
